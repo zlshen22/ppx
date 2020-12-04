d6aa008910a13fbea66abeffa664bd7bb8d99131
DESKTOP-1176HB0\zlshen <1129120844@qq.com>
2020-12-04 19:56:55 +0800

    Initial commit

---
+++ src/test/java/com/ppx/PpxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
